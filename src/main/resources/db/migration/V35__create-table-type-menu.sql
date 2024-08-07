273ca8acb3aaeb46879b043dc2124da75174306c
Ederson Sergio Coelho <edersonsergiocoelho@gmail.com>
2024-08-06 22:35:42 -0300

    HURR-78 - Criação Dos Script Do Menu.

---
+++ src/main/resources/db/migration/V35__create-table-type-menu.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS type_menu
+(
+    type_menu_id UUID DEFAULT uuid_generate_v4(), -- Identificador único do tipo de menu
+    type_menu_name CHARACTER VARYING(100) NOT NULL UNIQUE, -- Nome do tipo de menu
+    description TEXT NOT NULL, -- Descrição do tipo de menu
+    created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT current_timestamp, -- Data de criação do registro
+    modified_date TIMESTAMP WITHOUT TIME ZONE, -- Data de modificação do registro
+    enabled boolean NOT NULL DEFAULT true, -- Indicador de ativação do registro
+    CONSTRAINT type_menu_pkey PRIMARY KEY (type_menu_id) -- Chave primária da tabela (identificador único do tipo de menu)
+);
+
+COMMENT ON TABLE type_menu IS 'Tabela para armazenar os diferentes tipos de menus';
+
+COMMENT ON COLUMN type_menu.type_menu_id IS 'Identificador único do tipo de menu';
+COMMENT ON COLUMN type_menu.type_menu_name IS 'Nome do tipo de menu';
+COMMENT ON COLUMN type_menu.description IS 'Descrição do tipo de menu';
+COMMENT ON COLUMN type_menu.created_date IS 'Data de criação do registro';
+COMMENT ON COLUMN type_menu.modified_date IS 'Data de modificação do registro';
+COMMENT ON COLUMN type_menu.enabled IS 'Indicador de ativação do registro';
