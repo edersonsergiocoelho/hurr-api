13eef50b92547fe7a82015658e5fd19fb33e64f0
Ederson Sergio Coelho <edersonsergiocoelho@gmail.com>
2024-10-22 23:04:56 -0300

    HURR-99 - Script Para Acesso Ao Menu.

--- sql/V3.3__insert-menu-type-menu-menu-header-dropdown.sql
+++ sql/V3.3__insert-menu-type-menu-menu-header-dropdown.sql
@@ -10,3 +10,8 @@ INSERT INTO menu (name, description, icon, url, menu_order)
 VALUES
 ('Torne-Se Um Parceiro', 'Torne-Se Um Parceiro', 'pi pi-shop', 'user-role/edit/role', 2)
 ON CONFLICT (name) DO NOTHING; -- Ignora a inserção em caso de conflito pelo nome
+
+INSERT INTO menu (name, description, icon, url, menu_order)
+VALUES
+('Configurações', 'Configurações', 'pi pi-cog', 'settings/profile', 3)
+ON CONFLICT (name) DO NOTHING; -- Ignora a inserção em caso de conflito pelo nome
--- sql/V4.4__insert-role-menu-role-user.sql
+++ sql/V4.4__insert-role-menu-role-user.sql
@@ -36,6 +36,15 @@ VALUES (
     (SELECT menu_id FROM menu WHERE name = 'Torne-Se Um Parceiro'), -- Subconsulta para buscar o menu_id
     (SELECT type_menu_id FROM type_menu WHERE type_menu_name = 'MENU_HEADER_DROPDOWN') -- Subconsulta para buscar o type_menu_id
 )
+ON CONFLICT (role_id, menu_id, type_menu_id) DO NOTHING; -- Ignora a inserção em caso de
+
+-- Inserir uma associação básica na tabela 'role_menu' usando subconsultas para buscar os UUIDs
+INSERT INTO role_menu (role_id, menu_id, type_menu_id)
+VALUES (
+    (SELECT role_id FROM role WHERE role_name = 'ROLE_USER'), -- Subconsulta para buscar o role_id
+    (SELECT menu_id FROM menu WHERE name = 'Configurações'), -- Subconsulta para buscar o menu_id
+    (SELECT type_menu_id FROM type_menu WHERE type_menu_name = 'MENU_HEADER_DROPDOWN') -- Subconsulta para buscar o type_menu_id
+)
 ON CONFLICT (role_id, menu_id, type_menu_id) DO NOTHING; -- Ignora a inserção em caso de conflito
 
 -- Inserir uma associação básica na tabela 'role_menu' usando subconsultas para buscar os UUIDs
